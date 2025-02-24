71dbfe22053ca06f90c10d2735e8face61b9ba6b
Cesar Ramirez <cesaramirezfr@gmail.com>
2025-02-24 07:36:17 -0500

    move to category dir

--- LeetCode/TopInterview150/ArrayString/27-removeElement.test.ts
+++ LeetCode/TopInterview150/ArrayString/27-removeElement.test.ts
@@ -5,12 +5,12 @@ describe('removeElement', () => {
   it('should solve example 1', () => {
     const nums = [3,2,2,3];
     const val = 3;
-    expect(removeElement(nums, val)).toEqual(2);
+    expect(removeElement(nums, val)).toBe(2);
   });
 
   it('should solve example 2', () => {
     const nums = [0,1,2,2,3,0,4,2];
     const val = 2;
-    expect(removeElement(nums, val)).toEqual(5);
+    expect(removeElement(nums, val)).toBe(5);
   });
 });
